83493796c5b97b52888915f1ac41a0d763259888
Patrick Ring <patrick.ring@fastmail.com>
2025-08-01 23:41:29 +0000

    Added Resize, Rotate, and Translate to projection

--- projection.py
+++ projection.py
@@ -3,6 +3,7 @@ import math
 
 DEG_TO_RAD = math.pi / 180.0
 
+
 class ProjectionBase():
 
     def __init__(self):
@@ -13,27 +14,84 @@ class ProjectionBase():
 
     def deg_to_rad(self, coord):
         """Convert a coordinate tuple from degrees to radians.
+        Returns the converted tuple in radians.
         """
         return (coord[0] * DEG_TO_RAD, coord[1] * DEG_TO_RAD)
 
-    def project(self, coord_list):
-        """Transform the points according to the projection.
-        coord_list is list of x,y tuples in degrees.
+    def project_points(self, points):
+        """Transform a list of geographic coordinate system tuples.
+        points is a list of coordinate tuples in degrees.
+        Returns a list of cartesian coordinates tuples in the
+        selected map projection.
         """
-        
-        for coord in coord_list:
-            rad_tuple = self.deg_to_rad(coord)
-            map_coord = self.project_rad_tuple(rad_tuple)
-            
-        pass
+        pcs_points = []
+        for gcs_pt in points:
+            rad_pt = self.deg_to_rad(gcs_pt)
+            pcs_pt = self.project(rad_pt)
+            pcs_points.append(pcs_pt)
+        return pcs_points
 
-    def project_rad_tuple(self, rad_tuple):
-        """Transform a single coordinate tuple
+    def project(self, gcs_coord):
+        """Transform an x,y tuple according to the projection.
+        Convert from Geographic Coordinate System (GCS) to a
+        Projected Coordinate System (PCS)
+        gcs_coord is a coordinate tuple in radians.
+        Returns a cartesian coordinate tuple in the map projection.
         """
+        return gcs_coord
 
     def has_inverse(self):
         return False
 
     def invert(self):
         pass
+
+
+class Resize(ProjectionBase):
+
+    def __init__(self, cx, cy = None):
+        self.cx = cx
+        if (cy == None):
+            self.cy = cx
+        else:
+            self.cy = cy
+
+    def project(self, gcs_coord):
+        """Resize the coordinate.
+        """
+        return (gcs_coord[0] * self.cx,
+                gcs_coord[1] * self.cy)
+
+
+class Rotate(ProjectionBase):
+
+    def __init__(self, rad_angle):
+        self.angle = rad_angle
+        self.cos_a = math.cos(rad_angle)
+        self.sin_a = math.sin(rad_angle)
+
+    def project(self, gcs_coord):
+        """Rotate the coordinate.
+        """
+        x, y = gcs_coord
+        xp =  x * self.cos_a - y * self.sin_a
+        yp =  x * self.sin_a + y * self.cos_a
+        return (xp, yp)
+
+
+class Translate(ProjectionBase):
+
+    def __init__(self, cx, cy = None):
+        self.cx = cx
+        if (cy == None):
+            self.cy = cx
+        else:
+            self.cy = cy
+
+    def project(self, gcs_coord):
+        """Translate the coordinate.
+        """
+        return (gcs_coord[0] + self.cx,
+                gcs_coord[1] + self.cy)
+
 
